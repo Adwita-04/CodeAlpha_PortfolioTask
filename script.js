5b689d9dbe39b86a5627f5baaa05b79fd30e07b8
Adwita <adwita0411@gmail.com>
2025-07-29 19:17:13 +0530

    Implement resume view and download functionality

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
  const aboutLink = document.querySelector('a[href="#about"]');
   const aboutText = document.getElementById('aboutText');
-
+  const skillsLink=document.querySelector('a[href="#skills"]');
+const skillsContent = document.querySelector('.skills-section-content');
   aboutLink.addEventListener('click', () => {
     aboutText.classList.remove('fade-slide-up');
 
     // Use setTimeout to allow reflow naturally
     setTimeout(() => {
       aboutText.classList.add('fade-slide-up');
+    }, 50); // Small delay to ensure reflow
+  });
+  skillsLink.addEventListener('click', () => {
+    skillsContent.classList.remove('fade-slide-up');
+
+    // Use setTimeout to allow reflow naturally
+    setTimeout(() => {
+      skillsContent.classList.add('fade-slide-up');
     }, 50); // Small delay to ensure reflow
   });
   
